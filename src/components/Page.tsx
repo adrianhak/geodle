fa0eef0899e3fea033a39b81b6b0dedb5de18a56
Adrian Håkansson <adrian@adrianh.net>
2022-06-18 09:27:16 +0200

    Add page transitions

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,3 +1,4 @@
+import { Transition } from '@headlessui/react';
 import React, { useState } from 'react';
 import { X } from 'react-feather';
 import Modal from 'react-modal';
@@ -13,6 +14,7 @@ Modal.setAppElement('#root');
 
 const modalStyles = {
   overlay: {
+    background: 'rgba(0, 0, 0,0.8)',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -22,23 +24,32 @@ const modalStyles = {
 export const Page = (props: PageProps) => {
   return (
     <Modal
-      className='h-4/5 w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-neutral-100 dark:bg-neutral-900 z-50 items-center justify-center text-black dark:text-white p-2'
+      className='h-4/5 w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 z-50 items-center justify-center text-black dark:text-white'
       style={modalStyles}
       isOpen={props.showPage}
       onRequestClose={props.closePage}
       contentLabel={props.pageTitle}
       shouldCloseOnOverlayClick={true}>
-      <div className='text-center flex flex-col'>
-        <div className='flex items-center justify-center'>
-          <span className='text-lg font-semibold uppercase tracking-widest text-neutral-400 dark:text-neutral-500 flex-1 pl-7'>
-            {props.pageTitle}
-          </span>
-          <button onClick={props.closePage}>
-            <X className='w-7 text-neutral-400 dark:text-netural-500' />
-          </button>
+      <Transition
+        className='bg-neutral-100 dark:bg-black w-full h-full p-2 border border-neutral-400'
+        as='div'
+        appear={true} //THIS will make the transition run everytime the component is rendered
+        show={props.showPage}
+        enter='transition-all duration-200 ease-out'
+        enterFrom='transform translate-y-12 opacity-50'
+        enterTo='transform translate-y-0 opacity-100'>
+        <div className='text-center flex flex-col'>
+          <div className='flex items-center justify-center'>
+            <span className='text-lg font-semibold uppercase tracking-widest text-neutral-400 dark:text-neutral-300 flex-1 pl-7'>
+              {props.pageTitle}
+            </span>
+            <button onClick={props.closePage}>
+              <X className='w-7 text-neutral-400 dark:text-netural-300' />
+            </button>
+          </div>
+          {props.children}
         </div>
-        {props.children}
-      </div>
+      </Transition>
     </Modal>
   );
 };
