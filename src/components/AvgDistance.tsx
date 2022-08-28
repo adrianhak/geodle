3216cc9ebe1efae52f5c8174e923aeb9f309d4fb
Adrian Håkansson <adrian@adrianh.net>
2022-08-28 17:12:29 +0200

    Add support for imperial units

--- src/components/AvgDistance.tsx
+++ src/components/AvgDistance.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Units, useSettingsContext } from '../contexts/SettingsContext';
 
 interface AvgDistanceProps {
   distance: number | undefined;
 }
 
 export const AvgDistance = ({ distance }: AvgDistanceProps) => {
+  const { settings } = useSettingsContext();
+
+  const localizeDistance = (distance: number) => {
+    switch (settings.units) {
+      case Units.Metric:
+        return distance.toLocaleString() + ' km';
+      case Units.Imperial:
+        return Math.round(distance * 0.621371).toLocaleString() + ' mi';
+      default:
+        return distance.toLocaleString() + ' km';
+    }
+  };
+
   return (
     <div className='text-center'>
       <div className='font-semibold text-sm'>Average guess distance</div>
-      <div className='font-bold text-xl'> {distance ? distance.toLocaleString() : '-'} km</div>
+      <div className='font-bold text-xl'> {distance ? localizeDistance(distance) : '-'}</div>
     </div>
   );
 };
--- src/components/GuessRow.tsx
+++ src/components/GuessRow.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import CountUp from 'react-countup';
 import { ILocation } from '../@types/Location';
 import { Guess } from '../api';
+import { Units, useSettingsContext } from '../contexts/SettingsContext';
 import { locations } from '../locations';
 import { getCountryEmoji } from '../util/getCountryEmoji';
 
@@ -15,12 +16,20 @@ const findLocation = (code: string): ILocation | undefined => {
   return locations.find((location) => location.code === code);
 };
 
-const formatDistance = (distance: number) => {
-  // TODO: Support other units
-  return `${Math.round(distance).toLocaleString()}km`;
-};
-
 export const GuessRow = ({ guess, doCount, onCountDone }: GuessRowProps) => {
+  const { settings } = useSettingsContext();
+
+  const metricToImperial = (n: number) => n * 0.621371;
+
+  const formatDistance = useCallback((distance: number) => {
+    return Math.round(distance).toLocaleString();
+  }, []);
+
+  const distance =
+    settings.units === Units.Metric
+      ? guess?.distance
+      : Math.round(metricToImperial(guess?.distance || 0));
+
   return (
     <div className='mt-2 h-8 border-2 border-neutral-300 dark:border-neutral-600 text-neutral-900 dark:text-neutral-200 text-left px-1 flex justify-between items-center'>
       <div>
@@ -29,16 +38,22 @@ export const GuessRow = ({ guess, doCount, onCountDone }: GuessRowProps) => {
       <div>
         {guess &&
           (doCount ? (
-            <CountUp
-              start={20000}
-              end={guess.distance}
-              formattingFn={formatDistance}
-              duration={2}
-              useEasing={true}
-              onEnd={onCountDone}
-            />
+            <>
+              <CountUp
+                start={settings.units === Units.Metric ? 20000 : metricToImperial(20000)}
+                end={distance || 0}
+                formattingFn={formatDistance}
+                duration={2}
+                useEasing={true}
+                onEnd={onCountDone}
+              />
+              <span>{settings.units === Units.Metric ? 'km' : 'mi'}</span>
+            </>
           ) : (
-            formatDistance(guess.distance)
+            <>
+              {formatDistance(distance || 0)}
+              <span>{settings.units === Units.Metric ? 'km' : 'mi'}</span>
+            </>
           ))}
       </div>
     </div>
