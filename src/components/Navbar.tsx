231752417e6caa60e6fbe76d09cdf27d11ef63f6
Adrian Håkansson <adrian@adrianh.net>
2023-12-17 12:34:43 +0100

    Add Christmas decoration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,14 @@ const Navbar = () => {
           size={24}
           className='transition-transform ease-in-out hover:fill-red-700 duration-300 hover:text-red-700 hover:scale-125'></Heart>
       </button>
-      <h1 className='flex-auto font-notoserifDisplay text-3xl tracking-wider'>Geodle</h1>
+      <h1 className='flex-auto font-notoserifDisplay text-3xl tracking-wider xmas-text'>
+        <span>G</span>
+        <span>e</span>
+        <span>o</span>
+        <span>d</span>
+        <span>l</span>
+        <span>e</span>
+      </h1>
       <button className='ml-5' onClick={() => show(Page.Statistics, 0)}>
         <BarChart2 size={24} />
       </button>
